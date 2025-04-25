ed7de4d73f0f819c6f947c2d8136667b8022be19
Raunak Das <104708321+RaunakR16@users.noreply.github.com>
2025-04-25 21:05:41 +0530

    Update csv_To_BestFitCurve.py

--- DataDcreeningFromCSV/csv_To_BestFitCurve.py
+++ DataDcreeningFromCSV/csv_To_BestFitCurve.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+data = pd.read_csv('H:\Paper sensor with TANK\CODE FOR DATA SCREENING\DATA\sensor_data.csv', encoding='latin1')
+print(data.head())
+
 # Create a figure and axis
 fig, ax1 = plt.subplots(figsize=(12, 6))
 
